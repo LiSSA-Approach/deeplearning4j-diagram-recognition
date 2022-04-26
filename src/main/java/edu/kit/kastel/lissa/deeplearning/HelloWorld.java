14905aa07f4d4d9f8b6fc5849642a6d52dac0f42
Dominik Fuchß <develop@fuchss.org>
2022-04-26 11:10:21 +0300

    Initial commit

---
+++ src/main/java/edu/kit/kastel/lissa/deeplearning/HelloWorld.java
@@ -0,0 +1,13 @@
+/* Licensed under MIT 2022. */
+package edu.kit.kastel.lissa.deeplearning;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HelloWorld {
+    private static final Logger logger = LoggerFactory.getLogger(HelloWorld.class);
+
+    public static void main(String[] args) {
+        logger.info("Test");
+    }
+}
